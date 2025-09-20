6ca1bfffc6fe9cb7f2f6a34c6c7316ab2f62965d
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2025-09-20 09:02:43 +0200

    fix using multithread with memory db

--- py-user-server/data/src/data/db.py
+++ py-user-server/data/src/data/db.py
@@ -1,17 +1,20 @@
-from typing import Type
 from pathlib import Path
 from sqlalchemy import Engine, create_engine, event, Pool
-from sqlalchemy.pool.impl import QueuePool
+from sqlalchemy.pool import QueuePool, StaticPool
 
-def init_sqlite_engine(db_file: Path | None, poolclass: Type[Pool]|None = QueuePool) -> Engine:
+def init_sqlite_engine(db_file: Path | None, poolclass: type[Pool] | None = QueuePool, check_same_thread: bool = False) -> Engine:
     # To see why we do autocommit: False, see docs.sqlalchemy.org/en/20/dialects/sqlite.html#enabling-non-legacy-sqlite-transactional-modes-with-the-sqlite3-or-aiosqlite-driver
     # We use the default QueuePool
     if db_file is None:
         file_str = ":memory:"
+        # When using memory database, this is required
+        # https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#using-a-memory-database-in-multiple-threads
+        poolclass = StaticPool
+        check_same_thread = False
     else:
         file_str = str(db_file)
     engine = create_engine(
-        f"sqlite+pysqlite:///{file_str}", connect_args={"autocommit": False}, poolclass=poolclass
+        f"sqlite+pysqlite:///{file_str}", connect_args={"autocommit": False, "check_same_thread": check_same_thread}, poolclass=poolclass
     )
 
     # https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#foreign-key-support
